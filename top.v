b1905164f22495edb633ca87a68843ed77cd9a46
Q2CK <143080404+MaciejHeimowski@users.noreply.github.com>
2024-03-13 00:19:52 +0100

    niewykluczone że działa

--- top.v
+++ top.v
@@ -1,58 +1,72 @@
 `timescale 1ns / 1ns
 `include "debounce.v"
 `include "div.v"
+`include "display.v"
 
-module top(input clk_i, btnl_i, btnr_i, btnu_i, btnc_i);
-    wire l, r, u;
-    // L - ustawianie godzin
-    // R - ustawianie minut
-    // U - test 1000x
-    // C - reset
-    debounce #(50) l_db(clk_i, rst_i, btnl_i, l);
-    debounce #(50) r_db(clk_i, rst_i, btnr_i, r);
-    debounce #(50) u_db(clk_i, rst_i, btnu_i, u);
-
-    wire clk_1s, clk_main;
-
-    div #(1000) divider(clk_i, rst_i, clk_1s);
-
-    assign clk_main = u ? clk_i : clk_1s;
+module top #(parameter TPS = 10) (input clk_i, input btnl_i, input btnr_i, input btnu_i,  input rst_i, output [7:0] led7_seg_o, output [7:0] led7_an_o);
+    wire inc_hour, inc_minute, speedup;
+    debounce #(50) l_db(clk_i, rst_i, btnl_i, inc_hour);
+    debounce #(50) r_db(clk_i, rst_i, btnr_i, inc_minute);
+    debounce #(50) u_db(clk_i, rst_i, btnu_i, speedup);
 
     reg [5:0] h_val, m_val, s_val;
+
+    display disp(clk_i, rst_i, h_val, m_val, led7_seg_o, led7_an_o);
+
+    integer tick_counter;
+
+    initial $dumpvars(0, h_val, m_val, s_val);
 
     always @(posedge rst_i) begin
         if(rst_i) begin
             h_val <= 0;
             m_val <= 0;
             s_val <= 0;
-        end
-    end
 
-    always @(posedge clk_main) begin
-        $display(h_val, " ", m_val, " ", s_val);
-
-        s_val = s_val + 1;
-
-        if(s_val >= 6'd59) begin
-            s_val <= 0;
-            m_val <= m_val + 1;
-        end
-
-        if(m_val >= 6'd59) begin
-            m_val <= 0;
-            h_val <= h_val + 1;
-        end
-
-        if(h_val >= 23) begin
-            h_val <= 0;
-            m_val <= 0;
-            s_val <= 0;
+            tick_counter <= 0;
         end
     end
 
     always @(posedge clk_i) begin
-        if(l) h_val <= h_val + 1;
-        if(r) m_val <= m_val + 1;
+        if(tick_counter >= (speedup ? 1 : TPS - 1)) begin
+            tick_counter <= 0;
+
+            if(s_val >= 59) begin
+                s_val <= 0;
+                m_val <= m_val + 1;
+            end
+            else begin
+                s_val <= s_val + 1;
+            end
+
+            if(m_val >= 59 && s_val >= 59) begin
+                m_val <= 0;
+                h_val <= h_val + 1;
+            end
+
+            if(h_val >= 23 && m_val >= 59 && s_val >= 59) begin
+                h_val <= 0;
+                m_val <= 0;
+                s_val <= 0;
+            end
+        end
+        else begin
+            tick_counter <= tick_counter + 1;
+
+            if(tick_counter % (TPS / 5) == 0 && btnl_i == 1) begin
+                if(h_val < 23)
+                    h_val <= h_val + 1;
+                else
+                    h_val <= 0;
+            end
+
+            if(tick_counter % (TPS / 5) == 0 && btnr_i == 1) begin
+                if(m_val < 59)
+                    m_val <= m_val + 1;
+                else
+                    m_val <= 0;
+            end
+        end
     end
 
 endmodule
--- top_tb.v
+++ top_tb.v
@@ -2,20 +2,36 @@
 `include "top.v"
 
 module top_tb;
-    reg clk_i, rst_i, btn_i;
-    
-    reg btnl_i, btnr_i;
+    reg clk_i, rst_i;
+    reg btnl_i, btnr_i, btnu_i;
 
-    top rtc(clk_i, btnl_i, btnr_i, btnu_i, rst_i);
+    wire [7:0] led7_seg_o, led7_an_o;
+
+    top #(100) rtc(clk_i, btnl_i, btnr_i, btnu_i, rst_i, led7_seg_o, led7_an_o);
 
     initial begin
-        // $dumpfile("debounce.vcd");
-        // $dumpvars(0, clk_i, rst_i, btn_i);
+        $dumpfile("top.vcd");
+        $dumpvars(0, clk_i, btnl_i, btnr_i, btnu_i, rst_i, led7_seg_o, led7_an_o);
 
-        btnl_i = 0;
-        btnr_i = 0;
+        #0 rst_i = 0;
+        #0 clk_i = 0;
+        #0 btnl_i = 0;
+        #0 btnr_i = 0;
+        #0 btnu_i = 0;
 
-        #10000 $finish;
+        #1 rst_i = 1;
+        #1 rst_i = 0;
+
+        #1000 btnr_i = 1;
+        #1010 btnr_i = 0;
+
+        #2000 btnl_i = 1;
+        #2010 btnl_i = 0;
+
+        #3000 btnu_i = 1;
+        #3010 btnu_i = 0;
+
+        #100000 $finish;
     end
 
     always #1 clk_i <= ~clk_i;
